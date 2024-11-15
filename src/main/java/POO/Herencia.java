3f1996d28d7455a324d32dba8943ee5e7301cab5
danielsagastume11 <desagastume@uth.hn>
2024-11-15 15:37:08 -0600

    Terminacion del formulario Animales

--- src/main/java/POO/Herencia.java
+++ src/main/java/POO/Herencia.java
@@ -15,8 +15,10 @@ public class Herencia {
     public static void main(String[] args) {
     Animal perro1 = new Perro("Color Negro", "SI", "Si", "Si", 4);
     //perro1.Sonido();
+    //perro1.Sonido();
     
     Animal gato1 = new Gato("Pelo naranja", "Si", "Si", "Si", 4);
+        //Qeco1.Sonido();
     //gato1.Sonido();
     
     Animal Qeco1 = new Qeco("No", "Si", "Si", "SI", 4);
